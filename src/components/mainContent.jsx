dcc44be14d6a0ec4220ee8c168e9b140bd510a66
Egor Borisenko <agron.354@gmail.com>
2021-01-24 22:53:25 +0300

    Implemented styles for new block in main content

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -18,10 +18,22 @@ export function MainContent() {
                 </div>
             </div>
             <div className="container-fluid" style={{height: "40%"}}>
-                <div className="row justify-content-center" style={{height: "70vh"}}>
+                <div className="row" style={{height: "70vh"}}>
                     <div className="col-sm-6 w-50 h-100 main-content-img">
                     </div>
-                    <div className="col-sm-6 w-50 h-100" style={{backgroundColor: "#f5f4f4"}}>
+                    <div className="col-sm-6 w-50 h-100 " style={{backgroundColor: "#f5f4f4", padding: "0px 6% 0 6%"}}>
+                        <div className="row h-50 py-3 d-flex align-items-end" style={{textAlign: "left"}}>
+                            <span className="col-12 py-1 d-flex main-content-text-title">
+                                Hello! 
+                                I'm Egor Borisenko.
+                            </span>
+                        </div>
+                        <div className="row h-50 py-3 d-flex align-items-start" style={{textAlign: "left"}}>
+                            <span className="col-12 h-50 py-1 d-flex main-content-text">
+                                Web services, that allows users to view and manipulate with different data coming from IoT things,
+                                create statistics automation for users IoT zones.
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
