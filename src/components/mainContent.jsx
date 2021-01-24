e698a2e94413d2c81bfd859a99544066b99c1273
Egor Borisenko <agron.354@gmail.com>
2021-01-24 22:08:56 +0300

    Added new content block

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -4,7 +4,7 @@ import { ProjectItem } from './projectItem';
 export function MainContent() {
     return (
         <div className="main-content">
-            <div className="container h-50">
+            <div className="container" style={{height: "60%"}}>
                 <div className="row justify-content-center title second-screen-title-main">
                     <div className="col-sm-12">
                         <div>My commercial projects</div>
@@ -17,7 +17,13 @@ export function MainContent() {
                     <ProjectItem/>
                 </div>
             </div>
-            <div className="container-fluid h-50">
+            <div className="container-fluid" style={{height: "40%"}}>
+                <div className="row justify-content-center" style={{height: "70vh"}}>
+                    <div className="col-sm-6 w-50 h-100 main-content-img">
+                    </div>
+                    <div className="col-sm-6 w-50 h-100" style={{backgroundColor: "#f5f4f4"}}>
+                    </div>
+                </div>
             </div>
         </div>
     );
