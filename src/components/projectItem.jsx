0a5ca503e4e2d3b69168e81aecf4c49d5cc3068f
Egor Borisenko <agron.354@gmail.com>
2021-01-24 20:16:41 +0300

    Improved project styles

--- src/components/projectItem.jsx
+++ src/components/projectItem.jsx
@@ -5,16 +5,18 @@ export function ProjectItem() {
 
     return (
         <div 
-            className={`col-5 m-3 project-img justify-content-center ${descriptionIsVisible ? "description-background" : ""}`}
+            className={`col-5 col-xs-12 m-3 project-img justify-content-center ${descriptionIsVisible ? "description-background" : ""}`}
             onMouseOver={() => setDescriptionVisibility(true)} 
             onMouseLeave={() => setDescriptionVisibility(false)}>
-            <div className="hidden">
-                <span>Smart IoT management</span>
-                <span>
-                    Web services, that allows users to view and manipulate with different data coming from IoT things,
-                    create statistics automation for users IoT zones and things, 
-                    comparing of input and output parameters of IoT things.
-                </span>
+            <div className="hidden container">
+                <div className="row">
+                    <span className="col-12 py-1 d-flex justify-content-center align-items-end">Smart IoT management</span>
+                    <span className="col-12 py-1 d-flex justify-content-center">
+                        Web services, that allows users to view and manipulate with different data coming from IoT things,
+                        create statistics automation for users IoT zones and things, 
+                        comparing of input and output parameters of IoT things.
+                    </span>
+                </div>
             </div>
         </div>
     );
